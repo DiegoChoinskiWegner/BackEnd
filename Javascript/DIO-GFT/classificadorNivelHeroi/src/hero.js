701b96f06d8a381a8b1163304fe103cb887c38a7
DiegoChoinsiWegner <wegner.dcw@gmail.com>
2024-10-17 18:21:57 -0300

    alteração de dados de persistencia e Rank do heroi

--- Javascript/DIO-GFT/classificadorNivelHeroi/src/hero.js
+++ Javascript/DIO-GFT/classificadorNivelHeroi/src/hero.js
@@ -82,31 +82,96 @@ class Archer{
 }
 
 class Hero {
-  constructor(name, level, heroClass) { 
-    this.name = name;
-    this.level = level;
-
-    // Check if heroClass is a valid type (Warrior, Mage, Archer)
-    if (!(heroClass instanceof Warrior || heroClass instanceof Mage || heroClass instanceof Archer)) {
-      throw new Error("Invalid hero class provided!");
+    constructor(name, level, rank, experience, heroClass) { 
+        this.name = name;
+        this.level = level;
+        this.rank = rank;
+        this.experience = experience;
+    
+        // Check if heroClass is a valid type (Warrior, Mage, Archer)
+        if (!(heroClass instanceof Warrior || heroClass instanceof Mage || heroClass instanceof Archer)) {
+          throw new Error("Invalid hero class provided!");
+        }
+    
+        // Directly assign stats from the heroClass object
+        this.strength = heroClass.strength;
+        this.intelligence = heroClass.intelligence;
+        this.agility = heroClass.agility;
+        this.defense = heroClass.defense;
+        this.heroClass = heroClass; // Store the entire heroClass object for potential future use
     }
 
-    // Directly assign stats from the heroClass object
-    this.strength = heroClass.strength;
-    this.intelligence = heroClass.intelligence;
-    this.agility = heroClass.agility;
-    this.defense = heroClass.defense;
-    this.heroClass = heroClass; // Store the entire heroClass object for potential future use
-  }
+    // Método para calcular a experiência necessária para o próximo nível
+    experienciaNecessaria() {
+        while (this.level <= 10){
+            return this.level * 100;
+        }
+        while (this.level > 10 && this.level <= 25){
+            return this.level * 115;
+        }
+        while (this.level > 25 && this.level <= 50){
+            return this.level * 130;
+        }
+        while (this.level > 50){
+            return this.level * 150;
+        }
+    }
 
-  showHero() {
-    console.log(`Hero name: ${this.name}, Level: ${this.level}`);
-    console.log(`Type: ${this.heroClass.constructor.name}`); // Use constructor.name for class name
-    console.log(`Strength: ${this.strength}`);
-    console.log(`Intelligence: ${this.intelligence}`);
-    console.log(`Agility: ${this.agility}`);
-    console.log(`Defense: ${this.defense}`);
-  }
+    ganharExperiencia(exp) {
+        this.experiencia += exp;
+        while (this.experiencia >= this.experienciaNecessaria()) {
+            this.experiencia -= this.experienciaNecessaria();
+            this.levelUp();
+        }
+        this.salvar(); // Atualiza o herói no sessionStorage
+    }
+
+    levelUp() {
+        this.level++;
+        console.log(`${this.nome} subiu para o nível ${this.level}!`);
+    }
+
+    rankUp(level) {
+        if (level >= 4){
+            this.rank = "Ferro";
+        } else if (level >= 9){
+            this.rank = "Bronze";
+        }else if (level >= 15){
+            this.rank = "Prata";
+        }else if (level >= 22){
+            this.rank = "Ouro";
+        }else if (level >= 30){
+            this.rank = "Diamante";
+        }else if (level >= 40){
+            this.rank = "Esmeralda";
+        }else if (level >= 50){
+            this.rank = "Imortal";
+        }
+    }
+
+    saveHero() {
+        sessionStorage.setItem('hero', JSON.stringify(this));
+    }
+
+    // Método para carregar o herói do sessionStorage
+    static carregar() {
+        const dados = sessionStorage.getItem('hero');
+        if (dados) {
+            const { nome, level, experiencia } = JSON.parse(dados);
+            //return new Heroi(nome, level, experiencia);
+        }
+        return null;
+    }
+
+    showHero() {
+        console.log(`Hero name: ${this.name}, Level: ${this.level}`);
+        console.log(`Type: ${this.heroClass.constructor.name}`); // Use constructor.name for class name
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Intelligence: ${this.intelligence}`);
+        console.log(`Agility: ${this.agility}`);
+        console.log(`Defense: ${this.defense}`);
+        console.log(`Experience: ${this.experience}`);
+    }
 }
 
 export default Hero;
--- Javascript/DIO-GFT/classificadorNivelHeroi/src/script.js
+++ Javascript/DIO-GFT/classificadorNivelHeroi/src/script.js
@@ -3,6 +3,7 @@ import Hero, { Warrior, Mage, Archer } from "./hero.js";
 
 const showHero = document.getElementById('showHero');
 const formHero = document.getElementById('formHero');
+const gameContainer = document.getElementById('gameContainer');
 
 
 
@@ -14,16 +15,17 @@ function criarHeroi() {
 function salvarHeroi() {
     const nome = document.getElementById('nome').value;
     const tipo = document.getElementById('tipo').value;
-    const level = 1; // Valor padrão para o nível
+    let level = 1; // Valor padrão para o nível
+    let exp = 0;
 
     // Criando o herói com base no tipo
     let novoHeroi;
     if (tipo == 'mage') {
-        novoHeroi = new Hero(nome, level, new Mage());
+        novoHeroi = new Hero(nome, level, exp, new Mage());
     } else if (tipo == 'warrior') {
-        novoHeroi = new Hero(nome, level, new Warrior());
+        novoHeroi = new Hero(nome, level, exp, new Warrior());
     } else if (tipo == 'archer') {
-        novoHeroi = new Hero(nome, level, new Archer());
+        novoHeroi = new Hero(nome, level, exp, new Archer());
     } else {
         console.error('Tipo de herói inválido');
         return;
@@ -31,10 +33,61 @@ function salvarHeroi() {
 
     // Exibindo as informações do herói no console (pode ser substituído por qualquer outra ação)
     novoHeroi.showHero();
+
+    // Exibe o container de iniciar o Jogo
+    gameContainer.style.display = 'flex';
+    return novoHeroi;
 }
 
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+
+function dadosExperiencia(novoHeroi){
+
+    let levelHeroi = novoHeroi.level;
+    console.log(levelHeroi)
+    let expGanha = 0;
+
+
+    const tipoInimigo = getRandomNumber(1, 5);
+    console.log(tipoInimigo)
+    const levelInimigo = getRandomNumber(1, (levelHeroi + 3));
+    console.log(levelInimigo)
+    const sorte = getRandomNumber(1, 10);
+    console.log(sorte)
+
+    if (levelInimigo < levelHeroi && levelHeroi > 5){    
+        if(tipoInimigo == 1){
+            expGanha = novoHeroi.strength * levelInimigo;
+            console.log("Experiência ganha foi: " + expGanha)
+        }else if(tipoInimigo == 2){;
+            expGanha = novoHeroi.intelligence * levelInimigo;
+            console.log("Experiência ganha foi: " + expGanha)
+        }else if(tipoInimigo == 3){
+            expGanha = novoHeroi.agility * levelInimigo;
+            console.log("Experiência ganha foi: " + expGanha)
+        }else if(tipoInimigo == 4){
+            expGanha = novoHeroi.defense * levelInimigo;
+            console.log("Experiência ganha foi: " + expGanha)
+        }else if(tipoInimigo == 5){
+            expGanha = levelInimigo * sorte;
+            console.log("Experiência ganha foi: " + expGanha)
+        }
+    }else if(levelHeroi < 5){
+        expGanha = (levelInimigo * 1.6) * sorte;
+        console.log("Experiência ganha foi: " + expGanha)
+    } else{
+        expGanha = novoHeroi.exp * 0.95;
+        console.log("Você foi derrotado! Perca " + expGanha + " de experiência!") 
+    }
+
+    novoHeroi.exp = expGanha;
+}
 
 
 
 document.getElementById('criarHeroiButton').addEventListener('click', criarHeroi);
 document.getElementById('salvarHeroiButton').addEventListener('click', salvarHeroi);
+document.getElementById('iniciarJogoButton').addEventListener('click', dadosExperiencia);
